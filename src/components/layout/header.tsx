b01923c74d4215a1cde50231a6bbf37338fdc8b2
ION <thegoshagames@gmail.com>
2024-05-24 14:40:50 +0300

    Добавлена кнопка выхода из аккаунта

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,12 @@
+import { useLocation, useNavigate } from 'react-router-dom'
 import UserPhoto from '@/assets/images/user.png'
+import { routes } from '@/constants/routes'
 import { useGetCurrentUserQuery } from '@/redux/api/users'
 
 export function Header() {
+    const navigate = useNavigate()
+    const path = useLocation()
+
     const {
         data: user,
         isFetching: userFetching,
@@ -13,7 +18,13 @@ export function Header() {
     return (
         <header className="absolute top top-4 right-9">
             {successLoad && (
-                <div className="flex bg-white rounded-xl px-4 py-3 items-center border">
+                <div
+                    className="flex bg-white rounded-xl px-4 py-3 items-center border cursor-pointer"
+                    onClick={() =>
+                        path.pathname !== routes.SETTINGS &&
+                        navigate(routes.SETTINGS)
+                    }
+                >
                     <img src={UserPhoto} className="w-9 h-9 mr-6" />
                     <div>
                         <p className="font-semibold leading-5">{`${user?.person.last_name} ${user?.person.first_name}`}</p>
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react'
 import { t } from 'i18next'
 import { Fragment } from 'react/jsx-runtime'
+import { useNavigate } from 'react-router-dom'
 import { SettingsData, SettingsField } from './components/settings-field'
+import ConfirmForm from '@/components/confirm-form/confirm-form'
+import DialogWindow from '@/components/dialog-window/dialog-window'
 import ErrorLayout from '@/components/error-layout/error-layout'
 import { PageLayout } from '@/components/layout/page-layout'
+import Button from '@/components/ui/button'
+import { routes } from '@/constants/routes'
+import { useAppDispatch } from '@/hooks/reduxHooks'
+import { useErrorToast } from '@/hooks/use-error-toast'
+import { api } from '@/redux/api'
+import { useLogoutMutation } from '@/redux/api/auth'
 import { useGetCurrentUserQuery } from '@/redux/api/users'
+import { getJWTtokens, removeCookieValue } from '@/utils/cookie'
 
 export default function SettingsPage() {
+    const navigate = useNavigate()
+
+    const [confirmOpen, setConfirmOpen] = useState(false)
+
+    const dispatch = useAppDispatch()
+
     const {
         data: user,
         isFetching,
@@ -15,6 +32,37 @@ export default function SettingsPage() {
     } = useGetCurrentUserQuery()
 
     const successLoad = !isFetching && isSuccess
+
+    const [
+        logout,
+        {
+            error: logoutError,
+            isLoading: logoutLoading,
+            isSuccess: logoutSuccess,
+        },
+    ] = useLogoutMutation()
+
+    const handleLogout = () => {
+        const refreshToken = getJWTtokens().refreshToken
+
+        if (refreshToken) {
+            logout({ refresh_token: refreshToken! })
+        }
+
+        removeCookieValue('accessToken')
+    }
+
+    useEffect(() => {
+        if (logoutSuccess) {
+            removeCookieValue('refreshToken')
+            localStorage.clear()
+
+            dispatch(api.util.resetApiState())
+
+            navigate(routes.AUTH_PAGE, { replace: true })
+        }
+    }, [logoutSuccess])
+    useErrorToast(void 0, logoutError)
 
     return !error ? (
         <PageLayout title={t('page.settings')} className="h-full">
@@ -32,6 +80,31 @@ export default function SettingsPage() {
                         <SettingsField label={t('user.role')}>
                             <SettingsData data={user.role.role_name} />
                         </SettingsField>
+                        <DialogWindow
+                            open={confirmOpen}
+                            setOpen={setConfirmOpen}
+                            trigger={
+                                <Button
+                                    variant="ghost"
+                                    className="mt-4 mr-4 self-end px-10 text-red-500 hover:text-red-700"
+                                >
+                                    {t('log.out')}
+                                </Button>
+                            }
+                            header={
+                                <h1 className="text-xl font-bold">
+                                    {t('confirm.action')}
+                                </h1>
+                            }
+                            content={
+                                <ConfirmForm
+                                    setOpen={setConfirmOpen}
+                                    description={t('description.log.out')}
+                                    onConfirm={handleLogout}
+                                    loading={logoutLoading}
+                                />
+                            }
+                        />
                     </Fragment>
                 )}
             </div>
